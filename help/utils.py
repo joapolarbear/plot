ce460526135a0c77df8227422eaef0fe9cbbb17b
Hanpeng Hu <joapolarbear@gmail.com>
2023-07-18 21:19:27 +0800

    Plot for FL

--- help/utils.py
+++ help/utils.py
@@ -3,6 +3,41 @@ from matplotlib.lines import Line2D
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import math
+import numpy as np
+
+def fig_base(fig_num, row_first=True):
+    if row_first:
+        row_num = math.ceil(math.sqrt(fig_num))
+        col_num = math.ceil(fig_num / row_num)
+    else:
+        col_num = math.ceil(math.sqrt(fig_num))
+        row_num = math.ceil(fig_num / col_num)
+    return row_num * 100 + col_num * 10
+
+font_size = 36
+
+def reduce_tick_num(num, axis="y", low=1, high=1, type=None):
+    if axis == "y":
+        locs, labels = plt.yticks()
+    elif axis == "x":
+        locs, labels = plt.xticks()
+    else:
+        raise ValueError(axis)
+    _min = min(locs)
+    _max = max(locs)
+    _mid = (_min + _max) / 2
+    _range = (_max - _min)
+    low *= (_mid - 1.1 * _range / 2)
+    high *= _max
+    new_locs = np.arange(low, high, step=(high-low)/float(num))
+    new_locs = new_locs.astype(type)
+    # new_ticks = (new_locs / 1e4).astype(int)
+    if axis == "y":
+        plt.yticks(new_locs, fontsize=font_size-2)
+    else:
+        plt.xticks(new_locs, fontsize=font_size-2)
+        
 ### Refer to https://linuxtut.com/en/92c21048bacadce811ec/
 def set_hierarchical_xlabels(index, font_size, ax=None,
                              bar_xmargin=0.1, #Margins on the left and right ends of the line, X-axis scale
